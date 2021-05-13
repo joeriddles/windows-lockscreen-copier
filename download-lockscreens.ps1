80026875ce30345ec229323ce90f6f3801733284
Joseph Riddle <joeriddles10@gmail.com>
2021-05-13 13:25:49 -0700

    Change message

--- download-lockscreens.ps1
+++ download-lockscreens.ps1
@@ -2,7 +2,6 @@ param ($destination_folder)
 if ($null -eq $destination_folder) {
     $destination_folder = $HOME + "\Pictures\Lockscreens\"
 }
-Write-Host "Destination Path:" $destination_folder
 
 $source_path = $HOME + "\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\"
 $files = @(Get-ChildItem -Path $source_path)
@@ -11,4 +10,5 @@ $files.ForEach({
     $destination_path = $destination_folder + $_ + ".jpg"
     Copy-Item -Path $file_path -Destination $destination_path
 })
-Write-Host "Copied" $files.Count "files."
+
+Write-Host "Copied" $files.Count "files to" $destination_folder
